de4f9d38b52d4578d55b259380aab32e6ea1b41b
novrion <elindstenz@outlook.com>
2025-09-13 13:28:32 +0200

    Robberies functioning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 import random
+import math
 
 class UnitTest:
     @staticmethod
@@ -109,6 +110,8 @@ class Organisation:
         self.robbery_customer_spawn_probability_boost = 0.0
         self.robbery_customer_spawn_probability_drop = 0.0
         self.minutes_to_complete_task = 0
+        self.robbery_kill_probability = 0.0
+        self.robbery_spawn_prob_adj_coefficient = 1
 
         if not self.initialise_params(params_path):
             raise ValueError(f"Error initialising parameters with path: {params_path}")
@@ -117,6 +120,9 @@ class Organisation:
         self.current_time = TimeStamp(time=self.open.time)
         self.customer_count = 0
         self.total_customer_wait_time = 0
+
+        self.last_robbery_time = None
+        self.last_robbery_successful = None
 
     @staticmethod
     def assert_valid_probability(val):
@@ -172,6 +178,16 @@ class Organisation:
                 print("Invalid minutes_to_complete_task. Should be an integer greater than 0.")
                 return False
 
+            self.robbery_kill_probability = float(params[8])
+            if not self.assert_valid_probability(self.robbery_kill_probability):
+                print("Invalid robbery_kill_probability. Should be between 0 and 1.")
+                return False
+
+            self.robbery_spawn_prob_adj_coefficient = float(params[9])
+            if self.robbery_spawn_prob_adj_coefficient <= 0:
+                print("Invalid robbery_spawn_prob_adj_coefficient. Should be greater than 0.")
+                return False
+
         except:
             print(f"Error parsing parameters in file: {path}")
             return False
@@ -196,6 +212,41 @@ class Organisation:
         else:
             print(f"STATISTICS: {self.customer_count} customers, total customer wait time {self.total_customer_wait_time} minutes = {self.total_customer_wait_time // self.customer_count} minutes per customer")
 
+    def simulate_robbery(self):
+        n_kills = 0
+        for customer in self.queue:
+            self.total_customer_wait_time += self.current_time.time - customer.entry_time.time
+            if random.random() <= self.robbery_kill_probability:
+                n_kills += 1
+        self.queue.clear()
+        
+        print(f"Kl {self.current_time} A robber has entered the organisation! The queue has dispersed and {n_kills} customers have been killed!")
+        print(f"Madame Franco, who has a black belt in karate, tries to fight off the robbers, ", end='')
+
+        # Madame Franco succeeds or fails
+        if random.random() <= self.robbery_success_rate:
+            print("but fails.")
+            self.last_robbery_successful = True
+        else:
+            print("and succeeds!")
+            self.last_robbery_successful = False
+
+        self.last_robbery_time = TimeStamp(time=self.current_time.time)
+
+
+    def spawn_customer(self):
+        spawn_rate = self.customer_spawn_probability
+        if self.last_robbery_time:
+            time_diff = self.current_time.time - self.last_robbery_time.time - 1
+            probability_adjustment = -self.robbery_customer_spawn_probability_drop if self.last_robbery_successful else self.robbery_customer_spawn_probability_boost
+            spawn_rate = self.customer_spawn_probability + pow(math.e, -time_diff/self.robbery_spawn_prob_adj_coefficient) * probability_adjustment
+            if abs(spawn_rate - self.customer_spawn_probability) < 0.01:
+                self.last_robbery_time = None
+                self.last_robbery_successful = None
+
+        return random.random() <= spawn_rate
+
+
     def simulate(self):
         print("Starting simulation with parameters:")
         self.print_params()
@@ -215,8 +266,16 @@ class Organisation:
             if self.current_time == self.close:
                 print("Kl ", self.current_time, "stängs dörren")
 
+            # Robbery
+            if random.random() <= self.robbery_probability:
+                self.simulate_robbery()
+
+                # No new customers and all customers from the line are removed if robbery
+                self.current_time.add_minutes(1)
+                continue
+
             # New customer
-            if self.current_time < self.close and random.random() <= self.customer_spawn_probability:
+            if self.current_time < self.close and self.spawn_customer():
                 self.customer_count += 1
                 new_customer = Customer(self.customer_count, self.current_time)
                 if not self.queue:
