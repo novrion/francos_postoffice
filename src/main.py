f42224466a61f37b8a140decd6729715444a2407
novrion <elindstenz@outlook.com>
2025-09-14 20:27:34 +0200

    All but parameter selection & statistical summary done

--- src/main.py
+++ src/main.py
@@ -175,10 +175,17 @@ class GUI:
 
         self.screen = pygame.display.set_mode((width, height))
         self.clock = pygame.time.Clock()
-        self.font = pygame.font.Font("lib/Hack-Regular.ttf", 16)
 
-        self.bg = pygame.image.load("lib/bg.png")
-        self.screen.blit(self.bg, (0, 0))
+        self.font_IBM_2y = pygame.font.Font("assets/fonts/Mx437_IBM_CGA-2y.ttf", 16)
+        self.font_IBM = pygame.font.Font("assets/fonts/Mx437_IBM_CGA.ttf", 16)
+
+        self.bg0 = pygame.image.load("assets/backgrounds/bg0.png")
+        self.bg0_pressed = pygame.image.load("assets/backgrounds/bg0_pressed.png")
+        self.bg1 = pygame.image.load("assets/backgrounds/bg1.png")
+        self.bg1_hover = pygame.image.load("assets/backgrounds/bg1_hover.png")
+        self.bg1_clicked = pygame.image.load("assets/backgrounds/bg1_clicked.png")
+        self.bg2 = pygame.image.load("assets/backgrounds/bg2.png")
+        self.bg2_pressed = pygame.image.load("assets/backgrounds/bg2_pressed.png")
 
         self.logs = []
 
@@ -188,46 +195,134 @@ class GUI:
             self.logs.pop(0)
 
     def blit_logs(self):
-        x = self.width // 8
+        x = 240
+        y = 585
         for i, log in enumerate(self.logs):
-            text = self.font.render(log, True, self.BLACK)
+            text = self.font_IBM_2y.render(log, False, self.BLACK)
             text_rect = text.get_rect()
             text_rect.x = x
-            text_rect.y = self.height // (4/3) + text_rect.h * i
+            text_rect.y = y + text_rect.h * i
             self.screen.blit(text, text_rect)
 
-    def run(self, postoffice: PostOffice):
-        running = True
+    def blit_how_to(self):
 
-        while running:
+        # How to title
+        title_text = self.font_IBM.render("How To Play", False, self.BLACK)
+        title_text_rect = title_text.get_rect()
+        title_text_rect.x = 70
+        title_text_rect.y = 60
+        self.screen.blit(title_text, title_text_rect)
+
+        # How to instructions
+        raw_text = "Franco's Post Office simulates Madame Franco's post office. Every minute the store is open a customer may enter. Every customer has a randomised number of tasks that he or she needs to complete. If there are no other customers in the store Franco will get to work on the new customer's tasks right away. Otherwise the customer enters the queue. When Franco has completed all of a customer's tasks the customer leaves. Rarely the post office is robbed. Franco usually manages to fight off the robbers with her black belt in karate. Then the post office gets a PR boost. This increases the probability that customers visit her post office. Unfortunately, sometimes the robbers succeed. This decreases the probability that customers enter the post office. Adjust the parameters of the simulation and click PLAY to start the simulation. Then step through the simulation by pressing the SPACE key. At the end the statistics of the simulation will be summarised."
+        
+        # Wrap raw_text manually and blit lines
+        x = 70
+        y = 80
+        chars_per_line = 130
+        cut = False
+        last_cut_idx = 0
+        line_count = 0
+        for i in range(1, len(raw_text), 1):
+            if i % chars_per_line == 0:
+                cut = True
+            if i == len(raw_text) - 1 or (raw_text[i] in [' ', '\n', '\t'] and cut):
+                text = self.font_IBM_2y.render(raw_text[last_cut_idx:i+1].strip(), False, self.BLACK)
+                text_rect = text.get_rect()
+                text_rect.x = x
+                text_rect.y = y + text_rect.h * line_count
+                self.screen.blit(text, text_rect)
+
+                line_count += 1
+                last_cut_idx = i
+                cut = False
+
+
+    def blit_parameters(self):
+        pass
+
+    def run(self, postoffice: PostOffice):
+        stage = 0
+
+        while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    running = False
+                    pygame.quit()
+                    return
 
             self.screen.fill("black")
-            self.screen.blit(self.bg, (0,0))
 
-            # Simulation
+            #
+            # Franco's Post Office logic
+            #
             
-            key_pressed = False
+            delay_frame = 0
+
+            # Keyboard input
+            space_pressed = False
             keys = pygame.key.get_pressed()
             if keys[pygame.K_SPACE]:
-                key_pressed = True
-                postoffice.simulate(self)
+                space_pressed = True
 
-            self.blit_logs()
+            # Mouse input
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            left_click = pygame.mouse.get_pressed()[0]
 
-            # END Simulation
+            # Main menu
+            if stage == 0:
+                if space_pressed:
+                    self.screen.blit(self.bg0_pressed, (0, 0))
+                    stage = 1
+                    delay_frame = 300
+                else:
+                    self.screen.blit(self.bg0, (0, 0))
+
+            # Choose parameters before game
+            elif stage == 1:
+
+                # Play button
+                play_rect = pygame.Rect((958, 677), (195, 67))
+                hover_play = play_rect.collidepoint((mouse_x, mouse_y))
+                if hover_play and left_click:
+                    pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
+                    self.screen.blit(self.bg1_clicked, (0, 0))
+                    stage = 2
+                    delay_frame = 300
+                elif hover_play:
+                    pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
+                    self.screen.blit(self.bg1_hover, (0, 0))
+                else:
+                    pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
+                    self.screen.blit(self.bg1, (0, 0))
+
+                # Instructions
+                self.blit_how_to()
+
+                # Parameters
+                self.blit_parameters()
+
+            # Simulation
+            elif stage == 2:
+                if space_pressed:
+                    self.screen.blit(self.bg2_pressed, (0, 0))
+                    postoffice.simulate(self)
+                else:
+                    self.screen.blit(self.bg2, (0, 0))
+                self.blit_logs()
+
+            # End of game statistics
+            else:
+                pass
+
+
+            #
+            # END Franco's Post Office logic
+            #
 
             pygame.display.flip()
             self.clock.tick(60)
-
-            # Delay 500 milliseconds if key pressed to avoid spam
-            if key_pressed:
-                pygame.time.delay(500)
-
-        pygame.quit()
-        quit()
+            if delay_frame:
+                pygame.time.delay(delay_frame)
 
 
 
