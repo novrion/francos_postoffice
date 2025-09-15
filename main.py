57998d179856369d3782e08fca9661b49e09845b
novrion <elindstenz@outlook.com>
2025-09-15 19:41:08 +0200

    progress?

--- main.py
+++ main.py
@@ -162,9 +162,31 @@ class PostOffice:
 
             self.current_time += 1
 
+class InputBox:
+    def __init__(self, x, y, w, h, font, bg_colour, text_colour):
+        self.rect = pygame.Rect(x, y, w, h)
+        self.rect_outline = pygame.Rect(x-2, y-2, w+4, h+4)
+        self.bg_colour = bg_colour
+        self.text_colour = text_colour
+        self.font = font
+        self.input_text = ""
+        self.taking_input = False
+
+    def hovering(self, mouse_x, mouse_y):
+        return self.rect.collidepoint(mouse_x, mouse_y)
+
+    def blit(self, screen):
+        # Background and outline
+        pygame.draw.rect(screen, self.text_colour, self.rect_outline)
+        pygame.draw.rect(screen, self.bg_colour, self.rect)
+
+        # Text
+        text = self.font.render(self.input_text, False, self.text_colour)
+        screen.blit(text, self.rect)
 
 class GUI:
-    BLACK = (0,0,0)
+    BLACK = (0, 0, 0)
+    WHITE = (255, 255, 255)
 
     def __init__(self, width, height):
         pygame.init()
@@ -187,68 +209,206 @@ class GUI:
         self.bg2 = pygame.image.load("assets/backgrounds/bg2.png")
         self.bg2_pressed = pygame.image.load("assets/backgrounds/bg2_pressed.png")
 
-        self.logs = []
+        # Stores all Rects and input boxes to blit to the current screen
+        self.rects = {}
+        self.input_boxes = []
+
+        # Game stage
+        self.stage = 0
+
+    def set_bg(self, background):
+        self.screen.blit(background, (0, 0))
 
     def log(self, a: str):
-        self.logs.append(a)
-        if len(self.logs) > 10:
-            self.logs.pop(0)
+        key = "logs"
 
-    def blit_logs(self):
+        # Check if no logs yet
+        logs_empty = not (key in self.rects.keys())
+
+        # Append new Rect to GUI Rect dictionary
         x = 240
         y = 585
-        for i, log in enumerate(self.logs):
-            text = self.font_IBM_2y.render(log, False, self.BLACK)
-            text_rect = text.get_rect()
-            text_rect.x = x
-            text_rect.y = y + text_rect.h * i
-            self.screen.blit(text, text_rect)
+        text = self.font_IBM_2y.render(a, False, self.BLACK)
+        text_rect = text.get_rect()
+        text_rect.x = 240
+        text_rect.y = 585
+        if logs_empty:
+            self.rects[key] = [(text, text_rect)]
+        else:
+            self.rects[key].append((text, text_rect))
+
+        # Only keep the 10 most recent logs
+        if not logs_empty and len(self.rects[key]) > 10:
+            self.rects[key].pop(0)
+        
+
+    def blit_logs(self):
+        key = "logs"
+        if key in self.rects.keys():
+            log_count = 0
+            for log in self.rects[key]:
+                text = log[0]
+                rect = log[1]
+                rect.y = 585 + rect.h * log_count
+                self.screen.blit(text, rect)
+                log_count += 1
+
+    def clicked_play_button(self, mouse_x, mouse_y, left_clicked):
+        # Mouse on button?
+        button_rect = pygame.Rect((958, 677), (195, 67))
+        hovering = button_rect.collidepoin((mouse_x, mouse_y))
+
+        if hovering and left_clicked:
+            pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
+            self.set_bg(self.bg1_clicked) 
+            return True
+
+        elif hovering:
+            pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
+            self.set_bg(self.bg1_hover)
+
+        else:
+            if pygame.mouse.get_cursor() == pygame.SYSTEM_CURSOR_HAND:
+                print("True")
+                pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
+            self.set_bg(self.bg1)
+
+        return False
 
     def blit_how_to(self):
+        key = "how_to"
 
-        # How to title
-        title_text = self.font_IBM.render("How To Play", False, self.BLACK)
-        title_text_rect = title_text.get_rect()
-        title_text_rect.x = 70
-        title_text_rect.y = 60
-        self.screen.blit(title_text, title_text_rect)
+        # Rects not generated yet
+        if not key in self.rects.keys():
 
-        # How to instructions
-        raw_text = "Franco's Post Office simulates Madame Franco's post office. Every minute the store is open a customer may enter. Every customer has a randomised number of tasks that he or she needs to complete. If there are no other customers in the store Franco will get to work on the new customer's tasks right away. Otherwise the customer enters the queue. When Franco has completed all of a customer's tasks the customer leaves. Rarely the post office is robbed. Franco usually manages to fight off the robbers with her black belt in karate. Then the post office gets a PR boost. This increases the probability that customers visit her post office. Unfortunately, sometimes the robbers succeed. This decreases the probability that customers enter the post office. Adjust the parameters of the simulation and click PLAY to start the simulation. Then step through the simulation by pressing the SPACE key. At the end the statistics of the simulation will be summarised."
-        
-        # Wrap raw_text manually and blit lines
-        x = 70
-        y = 80
-        chars_per_line = 130
-        cut = False
-        last_cut_idx = 0
-        line_count = 0
-        for i in range(1, len(raw_text), 1):
-            if i % chars_per_line == 0:
-                cut = True
-            if i == len(raw_text) - 1 or (raw_text[i] in [' ', '\n', '\t'] and cut):
-                text = self.font_IBM_2y.render(raw_text[last_cut_idx:i+1].strip(), False, self.BLACK)
+            # How to title
+            title_text = self.font_IBM.render("How To Play", False, self.BLACK)
+            title_text_rect = title_text.get_rect()
+            title_text_rect.x = 70
+            title_text_rect.y = 60
+            self.rects[key] = [(title_text, title_text_rect)]
+
+            # How to instructions
+            raw_text = "Franco's Post Office simulates Madame Franco's post office. Every minute the store is open a customer may enter. Every customer has a randomised number of tasks that he or she needs to complete. If there are no other customers in the store Franco will get to work on the new customer's tasks right away. Otherwise the customer enters the queue. When Franco has completed all of a customer's tasks the customer leaves. Rarely the post office is robbed. Franco usually manages to fight off the robbers with her black belt in karate. Then the post office gets a PR boost. This increases the probability that customers visit her post office. Unfortunately, sometimes the robbers succeed. This decreases the probability that customers enter the post office. Adjust the parameters of the simulation and click PLAY to start the simulation. Then step through the simulation by pressing the SPACE key. At the end the statistics of the simulation will be summarised."
+            
+            # Wrap raw_text manually and blit lines
+            x = 70
+            y = 80
+            chars_per_line = 130
+            cut = False
+            last_cut_idx = 0
+            line_count = 0
+            for i in range(1, len(raw_text), 1):
+                if i % chars_per_line == 0:
+                    cut = True
+                if i == len(raw_text) - 1 or (raw_text[i] in [' ', '\n', '\t'] and cut):
+                    text = self.font_IBM_2y.render(raw_text[last_cut_idx:i+1].strip(), False, self.BLACK)
+                    text_rect = text.get_rect()
+                    text_rect.x = x
+                    text_rect.y = y + text_rect.h * line_count
+                    self.rects[key].append((text, text_rect))
+
+                    line_count += 1
+                    last_cut_idx = i
+                    cut = False
+
+
+        # Blit rects
+        for rect in self.rects[key]:
+                self.screen.blit(rect[0], rect[1])
+
+    def blit_parameter_names(self):
+        key = "parameter_names"
+
+        # Generate parameter name rects
+        if key not in self.rects.keys():
+            parameter_names = [
+                "Opening time",
+                "Closing time",
+                "Probability a customer enters every minute",
+                "Number of minutes it takes Franco to finish a customer's task",
+                "Probability a robbery occurs every minute",
+                "Probability a robbery succeeds",
+                "Probability a customer is killed in a robbery",
+                "Probability boost a customer enters every minute after an unsuccessful robbery",
+                "Probability drop a customer enters every minute after a successful robbery",
+                "Coefficient to determine how long a probability boost or drop continues after a robbery"
+            ]
+
+            x = 70
+            y = 300
+            parameter_count = 0
+            self.rects[key] = []
+            for param_name in parameter_names:
+                text = self.font_IBM.render(param_name, False, self.BLACK)
                 text_rect = text.get_rect()
                 text_rect.x = x
-                text_rect.y = y + text_rect.h * line_count
-                self.screen.blit(text, text_rect)
+                text_rect.y = y + (text_rect.h + 2) * parameter_count
+                self.rects[key].append((text, text_rect))
+                parameter_count += 1
 
-                line_count += 1
-                last_cut_idx = i
-                cut = False
+        for param in self.rects[key]:
+            self.screen.blit(param[0], param[1])
 
+    def blit_parameter_inputs(self, mouse_x, mouse_y, left_clicked):
 
-    def blit_parameters(self):
-        pass
+        # Initialise parameter input boxes
+        if not self.input_boxes:
+            for param in self.rects["parameter_names"]:
+                rect = param[1]
+                box = InputBox(rect.x + rect.w + 20, rect.y, 100, rect.h, self.font_IBM_2y, self.WHITE, self.BLACK)
+                self.input_boxes.append(box)
 
+        hovering = False
+        for box in self.input_boxes:
+            if box.hovering(mouse_x, mouse_y):
+                hovering = True
+                if left_clicked:
+                    box.taking_input = True
+
+        # No box taking input
+        if not hovering and left_clicked:
+            for box in self.input_boxes:
+                box.taking_input = False
+
+        # Determine cursor shape
+        if hovering:
+            pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_IBEAM)
+        else:
+            if pygame.mouse.get_cursor() == pygame.SYSTEM_CURSOR_IBEAM:
+                pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
+
+        # Dislay input boxes
+        for box in self.input_boxes:
+            box.blit(self.screen)
+    
     def run(self, postoffice: PostOffice):
-        stage = 0
+        self.stage = 0
 
         while True:
+
+            # Check if an input box is taking keyboard input
+            active_box_idx = -1
+            for i in range(len(self.input_boxes)):
+                if self.input_boxes[i].taking_input:
+                    active_box_idx = i
+                    break
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     return
+
+                # Update input text of box if there is an active one
+                if active_box_idx != -1:
+                    if event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_BACKSPACE:
+                            if self.input_boxes[active_box_idx].input_text != "":
+                                self.input_boxes[active_box_idx].input_text = self.input_boxes[active_box_idx].input_text[:-1]
+                        else:
+                            char = event.unicode
+                            if char.isnumeric() or char.isalpha() or char in [',', '.', ':']:
+                                self.input_boxes[active_box_idx].input_text += event.unicode
 
             self.screen.fill("black")
 
@@ -266,54 +426,47 @@ class GUI:
 
             # Mouse input
             mouse_x, mouse_y = pygame.mouse.get_pos()
-            left_click = pygame.mouse.get_pressed()[0]
+            left_clicked = pygame.mouse.get_pressed()[0]
 
             # Main menu
-            if stage == 0:
+            if self.stage == 0:
                 if space_pressed:
-                    self.screen.blit(self.bg0_pressed, (0, 0))
-                    stage = 1
+                    self.set_bg(self.bg0_pressed)
+                    self.stage = 1
+                    self.rects.clear()
                     delay_frame = 300
                 else:
-                    self.screen.blit(self.bg0, (0, 0))
+                    self.set_bg(self.bg0)
 
-            # Choose parameters before game
-            elif stage == 1:
+            # Choose parameters before simulation 
+            elif self.stage == 1:
 
-                # Play button
-                play_rect = pygame.Rect((958, 677), (195, 67))
-                hover_play = play_rect.collidepoint((mouse_x, mouse_y))
-                if hover_play and left_click:
-                    pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
-                    self.screen.blit(self.bg1_clicked, (0, 0))
-                    stage = 2
+                # TODO: Write function to determine cursor shape by having a centralized way of storing objects drawn
+                # TODO: Write function "progress_stage()" which automatically handles all the changes made between each stage 
+                if self.clicked_play_button(mouse_x, mouse_y, left_clicked):
+                    self.rects.clear()
                     delay_frame = 300
-                elif hover_play:
-                    pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
-                    self.screen.blit(self.bg1_hover, (0, 0))
-                else:
-                    pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
-                    self.screen.blit(self.bg1, (0, 0))
-
+                    self.stage = 2
+                
                 # Instructions
                 self.blit_how_to()
 
-                # Parameters
-                self.blit_parameters()
+                # Parameter selection
+                self.blit_parameter_names()
+                self.blit_parameter_inputs(mouse_x, mouse_y, left_clicked)
 
             # Simulation
-            elif stage == 2:
+            elif self.stage == 2:
                 if space_pressed:
-                    self.screen.blit(self.bg2_pressed, (0, 0))
+                    self.set_bg(self.bg2_pressed)
                     postoffice.simulate(self)
                 else:
-                    self.screen.blit(self.bg2, (0, 0))
+                    self.set_bg(self.bg2)
                 self.blit_logs()
 
-            # End of game statistics
+            # End of simulation statistics
             else:
                 pass
-
 
             #
             # END Franco's Post Office logic
